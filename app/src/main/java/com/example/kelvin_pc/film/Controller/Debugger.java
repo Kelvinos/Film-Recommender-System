b9b0c41374df341915f2e7d860278fe697720c6b
Kelvin Hiorns <kelvin.hiorns@gmail.com>
2016-02-28 16:49:55 +0000

    started on recommendations setup

--- app/src/main/java/com/example/kelvin_pc/film/Controller/Debugger.java
+++ app/src/main/java/com/example/kelvin_pc/film/Controller/Debugger.java
@@ -2,9 +2,31 @@ package com.example.kelvin_pc.film.Controller;
 
 import android.util.Log;
 
+import Jama.Matrix;
+
 public class Debugger {
+
+    private String tag = "myTag";
+
     public void print(String string) {
-        Log.d("myTag", string);
+        Log.d(tag, string);
     }
-    public void print(String loc, String string) {Log.d("myTag " + loc, string);}
+    public void print(String loc, String string) {Log.d(tag + loc, string);}
+
+    public void printMatrix(String string, Matrix m) {
+        print(string);
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ ");
+        for (int i=0; i<m.getRowDimension(); i++) {
+            for (int j=0; j<m.getColumnDimension(); j++) {
+                sb.append(String.valueOf(m.get(i, j)) + " ");
+            }
+            sb.append("}");
+            Log.d(tag, sb.toString());
+            sb.setLength(0);
+            sb.append("{ ");
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/kelvin_pc/film/View/Recommendations.java
+++ app/src/main/java/com/example/kelvin_pc/film/View/Recommendations.java
@@ -5,17 +5,25 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.kelvin_pc.film.Controller.AsyncResponse;
+import com.example.kelvin_pc.film.Controller.Debugger;
+import com.example.kelvin_pc.film.Controller.Film_Downloader;
 import com.example.kelvin_pc.film.Model.Film;
 import com.example.kelvin_pc.film.Model.System_Variables;
 import com.example.kelvin_pc.film.Model.User;
 import com.example.kelvin_pc.film.R;
 import com.example.kelvin_pc.film.Model.BaseActivity;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Recommendations extends BaseActivity {
+import Jama.Matrix;
+
+public class Recommendations extends BaseActivity implements AsyncResponse {
 
     private User user;
     private HashMap<Film, Integer> ratings;
+    private Matrix xtr, xts, ytr, yts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,52 @@ public class Recommendations extends BaseActivity {
         if (ratings.size() < 20) {
             displayError();
         }
+        generateTrainingData();
+        generateTestData();
+    }
+
+    public void generateTrainingData() {
+        ArrayList<Film> ratedFilms = new ArrayList<>(ratings.keySet());
+        xtr = new Matrix(ratedFilms.size(), 3);
+        generateMatrix(xtr, ratedFilms);
+
+        ytr = new Matrix(ratedFilms.size(), 1);
+        generateLabels(ytr, ratedFilms);
+
+        new Debugger().printMatrix("TRAINING", xtr);
+        new Debugger().printMatrix("LABELS", ytr);
+    }
+
+    public void generateTestData() {
+        // 100 Popular films
+        Film_Downloader fd = new Film_Downloader();
+        fd.delegate = this;
+        fd.generateCategoryQuery("popular", Integer.toString(1));
+    }
+
+    @Override
+    public void processFinish(ArrayList<Film> films) {
+        xts = new Matrix(20, 3);
+        generateMatrix(xts, films);
+        new Debugger().printMatrix("TEST", xts);
+
+    }
+
+    public void generateMatrix(Matrix m, ArrayList<Film> films) {
+        for (int i=0; i<m.getRowDimension(); i++) {
+            Film f = films.get(i);
+            m.set(i, 0, Double.parseDouble(f.getRating()));
+            m.set(i, 1, Double.parseDouble(f.getRunTime()));
+            m.set(i, 2, Double.parseDouble(f.getReleaseDate().substring(0, 4)));
+        }
+    }
+
+    public void generateLabels(Matrix m, ArrayList<Film> films) {
+        for (int i=0; i<m.getRowDimension(); i++) {
+            Film f = films.get(i);
+            Double label =  1.0 * ratings.get(f);
+            m.set(i, 0, label);
+        }
     }
 
     public void displayError() {
@@ -46,4 +100,5 @@ public class Recommendations extends BaseActivity {
         l.setVisibility(View.GONE);
         t.setText("No suggestions." + "\n" +  "Must have rated at least 10 good films and 10 bad films.");
     }
+
 }
